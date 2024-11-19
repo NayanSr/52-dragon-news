e78fa1889866575f17cb41fdb070d1a0b088fb6f
Nayan Suter <nayankbps@gmail.com>
2024-11-20 00:11:24 +0600

    use a loading state to show spinner

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -9,27 +9,31 @@ const auth = getAuth(app);
 // eslint-disable-next-line react/prop-types
 const AuthProvider = ({children}) => {
     const [user,setUser] = useState(null);
+    const [isLoading, setIsLoading]= useState(true);
     console.log(user);
 
     const createNewUser = (email,password)=>{
+        setIsLoading(true);
         return createUserWithEmailAndPassword(auth,email,password)
     }
 
     const logInUser =(email,password)=>{
+        setIsLoading(true);
         return signInWithEmailAndPassword(auth,email,password)
     }
 
     const logOutUser =()=>{
+        setIsLoading(true);
         return signOut(auth)
     }
 
     const authInfo={
-        user, setUser, createNewUser, logInUser, logOutUser
+        user, setUser, createNewUser, logInUser, logOutUser, isLoading
     }
 
     // Obsurver
     useEffect(()=>{
-        const unsubscribe= onAuthStateChanged(auth,(currentUser)=>{setUser(currentUser)});
+        const unsubscribe= onAuthStateChanged(auth,(currentUser)=>{setUser(currentUser); setIsLoading(false)});
         return ()=>{unsubscribe}
     },[])
 
--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -4,7 +4,11 @@ import { Navigate } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const PrivateRoutes = ({children}) => {
-const{user}= useContext(AuthContext);
+const{user, isLoading}= useContext(AuthContext);
+
+    if(isLoading){
+       return <h2 className="text-4xl text-white bg-green-600 p-8 w-1/2 mx-auto text-center mt-12">LOADING........</h2>
+    }
 
     if(user && user?.email){
         return children
